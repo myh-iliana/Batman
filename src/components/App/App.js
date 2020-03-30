379314453a7943b88dcfdbfa36fbf58eada181d4
Myh_Iliana <bonobono000011@gmail.com>
2020-03-30 13:43:41 +0300

    finish header component, responsive header

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.scss';
+import Header from '../Header/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import s from './Header.module.scss';
+import Container from '../elements/Container/Container';
+import SearchIcon from '../../img/search-icon.svg';
+import Bookmark from '../../img/bookmark.svg';
+import Avatar from '../../img/avatar.png';
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <Container>
+        <div className={s.content}>
+          <div className={s.logo}>КиноПульс</div>
+          <form action="" className={s.search}>
+            <input type="search" placeholder="Фильмы, сериалы, актеры" />
+            <button type="submit" className="search__button">
+              <img src={SearchIcon} alt="search icon" />
+            </button>
+          </form>
+          <div className={s.user}>
+            <img src={Bookmark} alt="" className={s.user__bookmark} />
+            <img src={Avatar} alt="" className={s.user__avatar} />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
